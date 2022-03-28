873831e691f6312f924e9d0f683f0c7a462b0c86
MaxHeuillet <maxime.heuillet.france@gmail.com>
2022-03-28 19:46:55 -0400

    initial pipeline

---
+++ attacks.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+import numpy as np
+
+def fgsm(model, X, y, epsilon=0.1):
+    """ Construct FGSM adversarial examples on the examples X"""
+    delta = torch.zeros_like(X, requires_grad=True)
+    loss = nn.CrossEntropyLoss()(model(X + delta), y)
+    loss.backward()
+    return epsilon * delta.grad.detach().sign()
+
+def pgd_linf(model, X, y, epsilon=0.1, alpha=0.01, num_iter=5, randomize=False):
+    """ Construct FGSM adversarial examples on the examples X"""
+    if randomize:
+        delta = torch.rand_like(X, requires_grad=True)
+        delta.data = delta.data * 2 * epsilon - epsilon
+    else:
+        delta = torch.zeros_like(X, requires_grad=True)
+        
+    for t in range(num_iter):
+        loss = nn.CrossEntropyLoss()(model(X + delta), y)
+        loss.backward()
+        delta.data = (delta + alpha*delta.grad.detach().sign()).clamp(-epsilon,epsilon)
+        delta.grad.zero_()
+    return delta.detach()
+
