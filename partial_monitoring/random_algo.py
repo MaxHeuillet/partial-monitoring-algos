81633896157498bd5f326c0aec8ed18870c1821b
Maxime Heuillet <maximeheuillet@MacBook-Pro-de-Maxime.local>
2022-08-13 21:36:09 -0400

    contextual

--- partial_monitoring/random_algo.py
+++ partial_monitoring/random_algo.py
@@ -10,13 +10,13 @@ class Random():
         self.game = game
         self.horizon = horizon
 
-    def get_action(self, t):
+    def get_action(self, t, context = None ):
         
         pbt = np.ones( self.game.n_actions ) / self.game.n_actions
         action = np.random.choice(self.game.n_actions, 1,  p = pbt )[0]
         return action
 
-    def update(self, action, feedback, outcome, t):
+    def update(self, action, feedback, outcome, context, t):
         pass
 
     def reset(self,):
