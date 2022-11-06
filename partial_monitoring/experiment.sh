437403fe1862c18241f8d4503d1bc73ea06a186e
MaxHeuillet <maxime.heuillet.france@gmail.com>
2022-11-06 12:09:12 -0500

    figure visualisations

--- partial_monitoring/experiment.sh
+++ partial_monitoring/experiment.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+#SBATCH --account=def-adurand   # some account
+#SBATCH --time=10:00:00        # specify time limit (D-HH:MM)
+#SBATCH --cpus-per-task=50     # specify number threads
 
 python3 ./partial_monitoring/evaluation.py
