7ece172415bd8c1a5e46b4362bc08cef033d2268
Maxime Heuillet <mheuill@beluga1.int.ets1.calculquebec.ca>
2022-11-08 10:40:36 -0500

    debug slurm

--- partial_monitoring/experiment.sh
+++ partial_monitoring/experiment.sh
@@ -6,7 +6,12 @@
 #SBATCH --mail-user=maxime.heuillet.1@ulaval.ca
 #SBATCH --mail-type=ALL
 
-module load nixpkgs/16.09  gcc/7.3.0 ipopt/3.12.13
+
+
+module purge
+
+module load nixpkgs/16.09 
+#gcc/7.3.0 ipopt/3.12.13
 
 module load python/3.7 
 module load scipy-stack
@@ -15,7 +20,9 @@ module load StdEnv/2016
 module load gurobi/9.0.1
 
 
-source /home/mheuill/projects/def-adurand/mheuill/MYENV/bin/activate
+source /home/mheuill/projects/def-adurand/mheuill/MYENV2/bin/activate
+
+#pip install scikit-learn --no-index
 
 echo "Threads ${SLURM_CPUS_ON_NODE:-1}" > gurobi.env   # set number of threads
 
