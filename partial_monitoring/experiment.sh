8935ab1b12aaa46549adecf5293236c7358e3a2a
MaxHeuillet <maxime.heuillet.france@gmail.com>
2022-11-24 17:42:46 -0800

    results contextual partial monitoring

--- partial_monitoring/experiment.sh
+++ partial_monitoring/experiment.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-adurand
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=48
-#SBATCH --time=05:00:00
+#SBATCH --time=06:00:00
 #SBATCH --mem-per-cpu=300M
 
 #SBATCH --mail-user=maxime.heuillet.1@ulaval.ca
