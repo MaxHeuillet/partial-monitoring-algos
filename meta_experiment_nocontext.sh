8d6000dbfab9d8d0efe74a2072d34bb24ff23449
MaxHeuillet <maxime.heuillet.france@gmail.com>
2023-02-20 15:17:29 -0500

    results non contextual

--- meta_experiment_nocontext.sh
+++ meta_experiment_nocontext.sh
@@ -14,10 +14,9 @@ for game in 'LE' 'AT'
                 for alg in 'RandCBP_1_5' 'RandCBP_18_5' 'RandCBP_116_5' 'RandCBP_132_5' 'RandCBP_1_10' 'RandCBP_18_10' 'RandCBP_116_10' 'RandCBP_132_10' 'RandCBP_1_20' 'RandCBP_18_20' 'RandCBP_116_20' 'RandCBP_132_20' 'RandCBP_1_100' 'RandCBP_18_100' 'RandCBP_116_100' 'RandCBP_132_100'
 
                     do
-		            echo 'horizon' $horizon 'nfolds' $nfolds 'CONTEXT_TYPE' $context_type 'GAME' $game 'TASK' $task 'ALG' $alg 'VAR' $var
+		            echo 'horizon' $horizon 'nfolds' $nfolds 'CONTEXT_TYPE' $context_type 'GAME' $game 'TASK' $task 'ALG' $alg
     
                     sbatch --export=ALL,HORIZON=$horizon,NFOLDS=$nfolds,GAME=$game,TASK=$task,ALG=$alg, ./partial_monitoring/experiment_nocontext.sh 
-                    ((var++))
                     done
                 
                 done
