e35b7cde0d64c8b688fba7c5ec43a9671f2f633f
MaxHeuillet <maxime.heuillet.france@gmail.com>
2023-02-20 13:48:47 -0500

    remove non-necessary files

--- application_to_images/notes.py
+++
@@ -1,8 +0,0 @@
-# fig.add_traces([ go.Scatter( x=dft.index, y=[ np.sqrt(x) for x in sequence ], name= r'$\frac{1}{n}$' ) ])
-
-
-# fig.update_layout(xaxis=dict(  title='Input Sequence', gridcolor='white', gridwidth=0.75, ),
-#                   yaxis=dict(  title='Cumulative Regret', gridcolor='white',  gridwidth=0.75,),
-#                   paper_bgcolor='rgb(243, 243, 243)',
-#                   plot_bgcolor='rgb(243, 243, 243)',
-#                   legend=dict(yanchor="top",y=1.1,xanchor="left",x=0.01) )
