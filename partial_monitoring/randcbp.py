1ad715208030476c527741872375e8ca391ddc7b
MaxHeuillet <maxime.heuillet.france@gmail.com>
2023-02-21 11:02:39 -0500

    update randcbp

--- partial_monitoring/randcbp.py
+++ partial_monitoring/randcbp.py
@@ -69,7 +69,7 @@ class RandCBP():
         self.memory_neighbors = {}
 
  
-    def get_action(self, t):
+    def get_action(self, t, context):
 
         if t <  1 * self.N:
             action = t 
