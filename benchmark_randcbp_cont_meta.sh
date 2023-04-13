c188225af406cde2aa4b0e1a7093c77cc790395e
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2023-04-13 11:30:14 -0400

    remove done

--- benchmark_randcbp_cont_meta.sh
+++ benchmark_randcbp_cont_meta.sh
@@ -19,8 +19,6 @@ for game in 'LE' 'AT'
                     sbatch --export=ALL,HORIZON=$horizon,NFOLDS=$nfolds,GAME=$game,TASK=$task,ALG=$alg, ./partial_monitoring/benchmark_randcbp_cont.sh 
                     done
                 
-                done
+            done
         
-        done
-
     done
