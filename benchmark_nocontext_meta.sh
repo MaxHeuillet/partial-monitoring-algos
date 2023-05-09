237df7e4b5a17773bf8283cd573b117f616307d9
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2023-05-09 12:33:56 -0400

    full no context

--- benchmark_nocontext_meta.sh
+++ benchmark_nocontext_meta.sh
@@ -3,14 +3,14 @@
 horizon=20000
 nfolds=96
 
-for game in  'AT' #'LE' 
+for game in  'AT' 'LE' 
 
     do 
 
-        for task in 'balanced' #'imbalanced'
+        for task in 'balanced' 'imbalanced'
 
             do
-            for alg in 'RandCBP_1_5' #'RandCBP_18_5' 'RandCBP_116_5' 'RandCBP_132_5' 'RandCBP_1_10' 'RandCBP_18_10' 'RandCBP_116_10' 'RandCBP_132_10' 'RandCBP_1_20' 'RandCBP_18_20' 'RandCBP_116_20' 'RandCBP_132_20' 'RandCBP_1_100' 'RandCBP_18_100' 'RandCBP_116_100' 'RandCBP_132_100'
+            for alg in 'RandCBP_1_5' 'RandCBP_18_5' 'RandCBP_116_5' 'RandCBP_132_5' 'RandCBP_1_10' 'RandCBP_18_10' 'RandCBP_116_10' 'RandCBP_132_10' 'RandCBP_1_20' 'RandCBP_18_20' 'RandCBP_116_20' 'RandCBP_132_20' 'RandCBP_1_100' 'RandCBP_18_100' 'RandCBP_116_100' 'RandCBP_132_100'
                     
                 do
 		        echo 'horizon' $horizon 'nfolds' $nfolds 'GAME' $game 'TASK' $task 'ALG' $alg 
