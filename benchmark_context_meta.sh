8d0f6d8bd640eac7c318240dc1f44b440a7f6054
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2023-09-15 18:26:34 -0400

    update

--- benchmark_context_meta.sh
+++ benchmark_context_meta.sh
@@ -15,9 +15,10 @@ for context_type in 'linear' #'quintic'
             for task in 'balanced' #'imbalanced'
 
                 do
-                
-                for alg in 'random'
-                # for alg in 'STAP' 'PGTS' 'PGIDSratio'
+
+                # for alg in 'random'
+                for alg in 'STAP'
+                # for alg in 'PGTS' 'PGIDSratio'
                 # for alg in 'CBPside' 'RandCBPside2_10_5_07' 'RandCBPside2_1_5_07' 'RandCBPside2_18_5_07' 'RandCBPside2_116_5_07' 'RandCBPside2_132_5_07' 'RandCBPside2_10_10_07' 'RandCBPside2_1_10_07' 'RandCBPside2_18_10_07' 'RandCBPside2_116_10_07' 'RandCBPside2_132_10_07' 'RandCBPside2_10_20_07' 'RandCBPside2_1_20_07' 'RandCBPside2_18_20_07' 'RandCBPside2_116_20_07' 'RandCBPside2_132_20_07' 'RandCBPside2_10_100_07' 'RandCBPside2_1_100_07' 'RandCBPside2_18_100_07' 'RandCBPside2_116_100_07' 'RandCBPside2_132_100_07'        
                     do
 		            echo 'horizon' $horizon 'nfolds' $nfolds 'CONTEXT_TYPE' $context_type 'GAME' $game 'TASK' $task 'ALG' $alg 'VAR' $var
